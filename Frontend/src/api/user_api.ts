01245eed11cf5854e7a0100d6af33c585fbd244d
Sayyed Sharuk <sayyedsharuk75@gmail.com>
2024-04-19 11:32:35 +0530

    Image Issue resolved

--- Frontend/src/api/user_api.ts
+++ Frontend/src/api/user_api.ts
@@ -1,10 +1,10 @@
 import { UpdateProfileFormDataType } from "@/TYPES";
 
-export const registerApi = async (formData: UpdateProfileFormDataType) => {
+export const updateProfileData = async (formData: UpdateProfileFormDataType) => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user`, {
       method: "POST",
-      body: JSON.stringify({ ...formData, imgUrl: "https://picsum.photos/200/300" }),
+      body: JSON.stringify({ ...formData }),
       headers: {
         "Content-Type": "application/json",
       },
